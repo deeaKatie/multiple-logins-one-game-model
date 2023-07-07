6ed53cc5b20cf10c50874f35a904be19d839febd
Deea <deea.lip@gmail.com>
2023-07-07 15:08:49 +0300

    fxml cleared

--- Client/src/main/java/controller/PlayController.java
+++ Client/src/main/java/controller/PlayController.java
@@ -30,9 +30,7 @@ import java.util.Objects;
 
 
 public class PlayController implements IObserver {
-    ObservableList<User> modelPlayers = FXCollections.observableArrayList();
-    ObservableList<Card> modelCards = FXCollections.observableArrayList();
-
+    ObservableList<User> modelLeaderboard = FXCollections.observableArrayList();
     private IServices service;
     private User loggedUser;
 
@@ -42,21 +40,14 @@ public class PlayController implements IObserver {
     Label gameStatusLabel;
     @FXML
     Button logOutButton;
-
-    // PS
     @FXML
-    ListView<User> playersListViewPS;
+    ListView<User> leaderboardListView;
     @FXML
-    ListView<Card> cardsListViewPS;
+    Label leaderboardLabel;
     @FXML
-    Label playersLabelPS;
-    @FXML
-    Label cardsLabelPS;
-
-    // SS
+    Label yourGameLabel;
     @FXML
     Button startGameButtonSS;
-
 
     boolean sentToWaiting = false;
 
@@ -67,42 +58,9 @@ public class PlayController implements IObserver {
         this.loggedUser = user;
         usernameLabel.setText("Hi, " + loggedUser.getUsername());
     }
-    public void init_PlayScreen(){
+    public void initVisuals() {
 
-        startGameButtonSS.setVisible(false);
-        playersListViewPS.setVisible(true);
-        cardsListViewPS.setVisible(true);
-        playersLabelPS.setVisible(true);
-        cardsLabelPS.setVisible(true);
     }
-    public void init_StartScreen(){
-        if (!sentToWaiting) {
-            startGameButtonSS.setVisible(true);
-            playersLabelPS.setVisible(false);
-            cardsLabelPS.setVisible(false);
-            playersListViewPS.setVisible(false);
-            cardsListViewPS.setVisible(false);
-            gameStatusLabel.setVisible(false);
-        }
-    }
-    public void init_WaitScreen(){
-        playersListViewPS.setVisible(false);
-        cardsListViewPS.setVisible(false);
-        gameStatusLabel.setText("Waiting...");
-        cardsLabelPS.setVisible(false);
-        playersLabelPS.setVisible(false);
-    }
-
-    @FXML
-    public void startGameHandler() {
-        try {
-            service.startGame(loggedUser.getId());
-        } catch (ServiceException ex) {
-            MessageAlert.showMessage(null, Alert.AlertType.ERROR,"Failed to start game", ex.getMessage());
-        }
-        System.out.println("Starting game!\n");
-    }
-
     @FXML
     public void logOutHandler() throws IOException {
         System.out.println("Logging out!\n");
@@ -122,119 +80,5 @@ public class PlayController implements IObserver {
         stage.setScene(scene);
     }
 
-    @Override
-    public void gameStarted(PlayersDTO players) {
-        System.out.println("CONTROLLER -> 1gameStarted");
-        Platform.runLater(()->{
-            System.out.println("CONTROLLER -> 2gameStarted");
-            // switch to game started view
-            init_PlayScreen();
 
-            modelPlayers.setAll(players.getPlayers());
-            playersListViewPS.setItems(modelPlayers);
-
-            modelCards.setAll(players.getDeck().getCards());
-            cardsListViewPS.setItems(modelCards);
-        });
-    }
-
-
-    public void sendToWaitingRoom() {
-        sentToWaiting = true;
-        init_WaitScreen();
-
-    }
-
-    @Override
-    public void gameFinished(String userStatus) {
-        Platform.runLater(() -> {
-            sentToWaiting = false;
-            init_StartScreen();
-            System.out.println("CTRL -> Received status: " + userStatus);
-            if (Objects.equals(userStatus, "won")) {
-                gameStatusLabel.setText("You won the game!");
-                gameStatusLabel.setVisible(true);
-
-                Deck deck = new Deck();
-                for(var c : modelCards) {
-                    deck.addCard(c);
-                }
-                try {
-                    System.out.println("CTRL -> Send winner cards");
-                    System.out.println("User: " + loggedUser);
-                    System.out.println("Deck: " + deck);
-                    WinnerDTO data = new WinnerDTO(loggedUser, deck);
-                    service.sendWinnerCards(data);
-                } catch (Exception e) {
-                    MessageAlert.showMessage(null, Alert.AlertType.ERROR,"Error logging out", e.getMessage());
-                }
-            }
-        });
-
-    }
-
-    public void sendSelectedCard(Card card) {
-        try {
-            UserMoveDTO move = new UserMoveDTO(loggedUser, card);
-            service.cardSelected(move);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void handleCardSelection(MouseEvent mouseEvent) {
-        Card card = cardsListViewPS.getSelectionModel().getSelectedItem();
-        sendSelectedCard(card);
-        cardsListViewPS.setDisable(true);
-    }
-
-    @Override
-    public void roundFinished(RoundEndDTO round) {
-        System.out.println("Round finished Controller");
-
-        Platform.runLater(()-> {
-            cardsListViewPS.setDisable(false);
-            gameStatusLabel.setVisible(true);
-
-            // add all cards if round winner
-            if (!round.getPlayerWon()) {
-                gameStatusLabel.setText("You Lost");
-                // remove card
-                for (var c : modelCards) {
-                    if (Objects.equals(c.getId(), round.getPlayedCard().getId())) {
-                        System.out.println("controller remove card: " + c.getValue());
-                        modelCards.remove(c);
-                        break;
-                    }
-                }
-            } else {
-                String cards = "";
-                for (var card : round.getRoundSelectedCards()) {
-                    if (card.getValue() != round.getPlayedCard().getValue()) {
-                        cards += card.getValue() + " | ";
-                        modelCards.add(card);
-                    }
-                }
-                gameStatusLabel.setText("You Won: " + cards);
-
-            }
-            cardsListViewPS.setItems(modelCards);
-
-            // ran out of cards
-            if (modelCards.isEmpty()) {
-                try {
-                    boolean status = service.noMoreCards(loggedUser);
-                    if (status) { // game ended
-                        gameFinished("");
-                    } else { // game still going go to waiting room
-                        sendToWaitingRoom();
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-
-    }
 }
--- Services/src/main/java/services/IObserver.java
+++ Services/src/main/java/services/IObserver.java
@@ -4,14 +4,12 @@ import dto.PlayersDTO;
 import dto.RoundEndDTO;
 
 public interface IObserver {
-    //void reservationMade(Reservation reservation) throws ServiceException;
-    void gameStarted(PlayersDTO players) throws ServiceException;
-
-    void sendToWaitingRoom();
-
-    void roundFinished(RoundEndDTO round);
-
-    void gameFinished(String userStatus);
-
+//    void gameStarted(PlayersDTO players) throws ServiceException;
+//
+//    void sendToWaitingRoom();
+//
+//    void roundFinished(RoundEndDTO round);
+//
+//    void gameFinished(String userStatus);
 
 }
